56d2679ad118b4de5be5eeea2c19884deaf183d0
GaussGun3000 <75047500+GaussGun3000@users.noreply.github.com>
2021-04-03 14:13:16 +0300

    Delete INT_Q_B.py

--- INT_Q_B.py
+++
@@ -1,9 +0,0 @@
-#number - массив из двух массивов. Первый массив - числитель, второй - знаменатель
-#функция проверяет, является ли число целым
-def INT_Q_B (number):
-    a = int(''.join((str(i) for i in number[0][1])))
-    b = int(''.join((str(i) for i in number[1][1])))
-    if a%b == 0:
-        return 'да'
-    else:
-        return 'нет'
