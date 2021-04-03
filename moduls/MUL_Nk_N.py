232f2c9a2f07bc3316f81048144863c8deb42c8d
GaussGun3000 <75047500+GaussGun3000@users.noreply.github.com>
2021-04-03 14:38:41 +0300

    Update MUL_Nk_N.py

--- moduls/MUL_Nk_N.py
+++ moduls/MUL_Nk_N.py
@@ -5,7 +5,5 @@ def MUL_Nk_N (number, deg):
         number[0] += deg
         for i in range (deg):
             number[1].append(0)
-    elif deg == 0:
-        number = [1, [1]]
     else:
         print("Неверное значение степени")
