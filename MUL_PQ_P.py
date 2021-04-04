2022db7a38f1fb09d316292d9884455172dcb463
GaussGun3000 <75047500+GaussGun3000@users.noreply.github.com>
2021-04-05 00:20:15 +0300

    Update MUL_PQ_P.py

--- MUL_PQ_P.py
+++ MUL_PQ_P.py
@@ -1,4 +1,4 @@
 def MUL_PQ_P(mult, polynomial):
     m = polynomial[0]
-    for i in range (m+1):
+    for i in range (m+1): #умножаем каждый коэффициент на рациональное число
         polynomial[1][i] = MUL_QQ_Q(polynomial[1][i], mult)
