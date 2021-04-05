df8b5b6a4e1bb03d4a622e5c4a9ab24ba0e307e9
bababasbebebe <81687162+bababasbebebe@users.noreply.github.com>
2021-04-05 09:13:41 +0300

    Update integer_nums.py

--- integer_nums.py
+++ integer_nums.py
@@ -47,7 +47,34 @@ def TRANS_Z_N (number):
         print("Ошибка! Задано отрицательное число")
 
 
-##ADD_ZZ_Z
+def ADD_ZZ_Z(mas1, mas2): #Сложение целых чисел
+    if POZ_Z_D(mas1) == 2 and POZ_Z_D(mas2) == 2:           #если оба числа положительны, складываем их модули, добавляем значение знака 0
+        result = ADD_NN_N(ABS_Z_N(mas1),ABS_Z_N(mas2))
+        result.insert(0,0)                                      
+    elif POZ_Z_D(mas1) == 1 and POZ_Z_D(mas2) == 1:         #если оба числа отрицательны, складываем их модули, добавляем значение знака 1
+        result = ADD_NN_N(ABS_Z_N(mas1),ABS_Z_N(mas2))
+        result.insert(0,1)
+    elif COM_NN_D(ABS_Z_N(mas1), ABS_Z_N(mas2)) == 0:       #если числа одинаковы по модулю и имею разные знаки, вывод 0
+        result = [0, 0, [0]]
+    elif POZ_Z_D(mas1) == 1 and POZ_Z_D(mas2) == 2:         #если 1е отрицательное, 2е положительное
+        if COM_NN_D(ABS_Z_N(mas1), ABS_Z_N(mas2)) == 2:         #1e по модулю больше, добавляем значение знака 1
+            result = SUB_NN_N(ABS_Z_N(mas1),ABS_Z_N(mas2))      
+            result.insert(0,1)
+        else:
+            result = SUB_NN_N(ABS_Z_N(mas2),ABS_Z_N(mas1))      #2e по модулю больше, добавляем значение знака 0
+            result.insert(0,0)
+    elif POZ_Z_D(mas1) == 2 and POZ_Z_D(mas2) == 1:
+        if COM_NN_D(ABS_Z_N(mas1), ABS_Z_N(mas2)) == 2:     #если 2е отрицательное, 1е положительное
+            result = SUB_NN_N(ABS_Z_N(mas1),ABS_Z_N(mas2))      #2e по модулю больше, добавляем значение знака 1    
+            result.insert(0,0)
+        else:
+            result = SUB_NN_N(ABS_Z_N(mas2),ABS_Z_N(mas1))      #1e по модулю больше, добавляем значение знака 0
+            result.insert(0,1)
+    elif POZ_Z_D(mas1) == 0:                                #1е число равно 0, значит присваиваем result 2е число
+        result = mas2
+    else:                                                   #2е число равно 0, значит присваиваем result 1е число
+        result = mas1                                           
+    return(result)  #вывод массива по шаблону (b, n; A[..]) - номер старшей позиции
 
 
 def SUB_ZZ_Z(num1, num2): #num1 - уменьшаемое, num2 - вычитаемое (!! Добвить return в TRANS_Z_N, иначе функция
