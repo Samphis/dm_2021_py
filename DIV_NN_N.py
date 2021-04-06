6142caf565e43cca1c510a36958541bd2bada4be
TOKAPb-VI <81686207+TOKAPb-VI@users.noreply.github.com>
2021-04-06 14:30:08 +0300

    Update DIV_NN_N.py

--- DIV_NN_N.py
+++ DIV_NN_N.py
@@ -6,10 +6,10 @@ def DIV_NN_N(mas1, mas2):
     for i in range(mas1[0]):
         num1 = num1 + mas1[1][i] * (10 ** (mas1[0] - i - 1))
     for i in range(mas2[0]):
-        num2 = num2 + mas2[1][i] * (10 ** (mas2[0] - i - 1))
+        num2 = num2 + mas2[1][i] * (10 ** (mas2[0] - i - 1)) #тут код Сани, вообще без изменений, см. его комы
     if COM_NN_D(mas1, mas2) == 1:
         quot = num2 // num1
-        ost = quot % 10
+        ost = quot % 10                                      #сэйвим последнию цифру числа, т.к она не попадет в цикл
         while quot // 10 != 0:
             count += 1
             quot = quot // 10
@@ -18,11 +18,11 @@ def DIV_NN_N(mas1, mas2):
         result[0] = count + 1
     else:
         quot = num1 // num2
-        ost = quot % 10
+        ost = quot % 10                                     #сэйвим последнию цифру числа, т.к она не попадет в цикл
         while quot // 10 != 0:
             count += 1
             quot = quot // 10
             result[1].append(quot)
         result[1].append(ost)
-        result[0] = count + 1
+        result[0] = count + 1                               #т.к count не учитывает наш последний разряд, то увеличиваем его на 1
     return result
